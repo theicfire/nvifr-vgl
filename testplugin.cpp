34ae1dacf6057f1847ba8cdb8e925bfe4587b02f
DRC <information@virtualgl.org>
2020-05-21 23:23:54 -0500

    Restructure plugin using a transport class
    This makes the plugin thread-safe as well as safe to use with
    applications that create multiple OpenGL windows.

--- testplugin.cpp
+++ testplugin.cpp
@@ -1,29 +1,112 @@
+// This file contains code based on the VirtualGL example image transport
+// plugins, which are:
+//
+// Copyright (C)2009-2011, 2014, 2017-2020 D. R. Commander
+//
+// The code in question has been re-licensed, with permission of the author,
+// from the wxWidgets Library License v3.1 to the business-friendly BSD-style
+// license listed below.  All other code contributions in this file from the
+// same author retain the same copyright and are provided under the same
+// license.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - Neither the name of The VirtualGL Project nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS",
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
 #include "rrtransport.h"
 #include "Error.h"
-// #include "./encoder/encoder.h"
 #include "nvifr-encoder/XCapture.h"
 
 using namespace vglutil;
 
-int get_frame_count = 0;
-
 static Error err;
-char errStr[MAXSTR + 14], errStrTmp[256];
-uint8_t thing[10];
-FILE *m_fpOut;
-
-RRFrame rr_frame;
-NV_IFROGL_SESSION_HANDLE m_hSession;
+static char errStr[MAXSTR + 14];
 static NV_IFROGL_HW_ENC_TYPE codecType = NV_IFROGL_HW_ENC_H264;
-NV_IFROGL_TRANSFEROBJECT_HANDLE m_hTransferObject = nullptr;
-NV_IFROGL_HW_ENC_CONFIG config;
-NV_IFROGL_TO_SYS_CONFIG to_sys_config;
-NV_IFROGL_TRANSFEROBJECT_HANDLE m_hSysTransferObject = nullptr;
 
-void throw_nvifr_error(const char *msg)
+class GPUEncTrans
+{
+	public:
+		GPUEncTrans(Display *dpy_, Window win_, FakerConfig *fconfig_) :
+			m_fpOut(NULL), m_hSession(NULL), m_hTransferObject(NULL),
+			m_hSysTransferObject(NULL), dpy(dpy_), win(win_), fconfig(fconfig_)
+		{
+			memset(&rr_frame, 0, sizeof(RRFrame));
+			char tmp[256];
+			snprintf(tmp, 256, "%lx.h264", win);
+			m_fpOut = fopen(tmp, "wb");
+			if (m_fpOut == NULL)
+			{
+				THROW("Failed to create output file.");
+			}
+		}
+
+		~GPUEncTrans()
+		{
+			if (m_hTransferObject)
+				XCapture::nvIFR.nvIFROGLDestroyTransferObject(m_hTransferObject);
+			if (m_hSysTransferObject)
+				XCapture::nvIFR.nvIFROGLDestroyTransferObject(m_hSysTransferObject);
+			if (m_hSession) XCapture::nvIFR.nvIFROGLDestroySession(m_hSession);
+			if (rr_frame.bits) delete [] rr_frame.bits;
+			if (m_fpOut) fclose(m_fpOut);
+		}
+
+		RRFrame *getFrame(int width, int height, int format)
+		{
+			rr_frame.w = width;
+			rr_frame.h = height;
+			rr_frame.pitch = width * rrtrans_ps[format];
+			if (rr_frame.bits) delete [] rr_frame.bits;
+			rr_frame.bits = new unsigned char[height * rr_frame.pitch];
+			rr_frame.rbits = nullptr;
+			rr_frame.format = format;
+			return &rr_frame;
+		}
+
+		void setupNVIFRSYS();
+		void setupNVIFRHwEnc(int width, int height);
+		void captureHwEnc();
+		void captureSys();
+
+	private:
+		void throw_nvifr_error(const char *msg);
+		void initNVIFR();
+
+		char errStrTmp[256];
+		FILE *m_fpOut;
+		RRFrame rr_frame;
+		NV_IFROGL_SESSION_HANDLE m_hSession;
+		NV_IFROGL_TRANSFEROBJECT_HANDLE m_hTransferObject;
+		NV_IFROGL_TRANSFEROBJECT_HANDLE m_hSysTransferObject;
+		Display *dpy;
+		Window win;
+		FakerConfig *fconfig;
+};
+
+void GPUEncTrans::throw_nvifr_error(const char *msg)
 {
 	unsigned int returnedLen, remainingBytes;
 	snprintf(errStrTmp, 256, "%s\nnvifrogl error: ", msg);
@@ -32,13 +115,30 @@ void throw_nvifr_error(const char *msg)
 	THROW(errStrTmp);
 }
 
-void setupNVIFRSYS()
+void GPUEncTrans::initNVIFR()
 {
+	if (m_hSession) return;
+
+	XCapture::nvIFR.initialize();
+	if (XCapture::nvIFR.nvIFROGLCreateSession(&m_hSession, NULL) !=
+		NV_IFROGL_SUCCESS)
+	{
+		THROW("Failed to create a NvIFROGL session.");
+	}
+}
+
+void GPUEncTrans::setupNVIFRSYS()
+{
+	initNVIFR();
+
+	if (m_hSysTransferObject) return;
+
+	NV_IFROGL_TO_SYS_CONFIG to_sys_config;
+	memset(&to_sys_config, 0, sizeof(NV_IFROGL_TO_SYS_CONFIG));
 	to_sys_config.format = NV_IFROGL_TARGET_FORMAT_YUV420P;
 	int ret;
-	if ((ret = XCapture::nvIFR.nvIFROGLCreateTransferToSysObject(
-			 m_hSession, &to_sys_config, &m_hSysTransferObject)) !=
-		NV_IFROGL_SUCCESS)
+	if ((ret = XCapture::nvIFR.nvIFROGLCreateTransferToSysObject(m_hSession,
+		&to_sys_config, &m_hSysTransferObject)) != NV_IFROGL_SUCCESS)
 	{
 		snprintf(errStrTmp, 256,
 			"Lame! NvIFROGLCreateRawSession Failed to create a transfer object "
@@ -47,35 +147,39 @@ void setupNVIFRSYS()
 	}
 }
 
-void setupNVIFRHwEnc(int width, int height)
+void GPUEncTrans::setupNVIFRHwEnc(int width, int height)
 {
-	if (!config.profile) {
-		config.profile = 100;
-		config.frameRateNum = 30;
-		config.frameRateDen = 1;
-		config.width = width;
-		config.height = height;
-		config.avgBitRate = width * height * 30 * 12 / 8;
+	initNVIFR();
 
-		config.GOPLength = 75;
-		config.rateControl = NV_IFROGL_HW_ENC_RATE_CONTROL_CBR;
-		config.stereoFormat = NV_IFROGL_HW_ENC_STEREO_NONE;
-		config.VBVBufferSize = config.avgBitRate;
-		config.VBVInitialDelay = config.avgBitRate;
-		config.codecType = codecType;
+	if (m_hTransferObject) return;
 
-		if (XCapture::nvIFR.nvIFROGLCreateTransferToHwEncObject(
-				m_hSession, &config, &m_hTransferObject) != NV_IFROGL_SUCCESS)
-		{
-			snprintf(errStrTmp, 256,
-				"Failed to create a NvIFROGL transfer object. w=%d, h=%d\n",
-				config.width, config.height);
-			THROW(errStrTmp);
-		}
+	NV_IFROGL_HW_ENC_CONFIG config;
+	memset(&config, 0, sizeof(NV_IFROGL_HW_ENC_CONFIG));
+	config.profile = 100;
+	config.frameRateNum = 30;
+	config.frameRateDen = 1;
+	config.width = width;
+	config.height = height;
+	config.avgBitRate = width * height * 30 * 12 / 8;
+
+	config.GOPLength = 75;
+	config.rateControl = NV_IFROGL_HW_ENC_RATE_CONTROL_CBR;
+	config.stereoFormat = NV_IFROGL_HW_ENC_STEREO_NONE;
+	config.VBVBufferSize = config.avgBitRate;
+	config.VBVInitialDelay = config.avgBitRate;
+	config.codecType = codecType;
+
+	if (XCapture::nvIFR.nvIFROGLCreateTransferToHwEncObject(m_hSession, &config,
+		&m_hTransferObject) != NV_IFROGL_SUCCESS)
+	{
+		snprintf(errStrTmp, 256,
+			"Failed to create a NvIFROGL transfer object. w=%d, h=%d\n",
+			config.width, config.height);
+		THROW(errStrTmp);
 	}
 }
 
-void captureHwEnc()
+void GPUEncTrans::captureHwEnc()
 {
 	GLint drawFBO;
 	glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &drawFBO);
@@ -83,25 +187,18 @@ void captureHwEnc()
 	GLenum readBuffer;
 	glGetIntegerv(GL_READ_BUFFER, (GLint *)&readBuffer);
 
-	fprintf(stderr, "fbo is %d, readBuffer is %d\n", drawFBO, readBuffer);
-
 	uintptr_t dataSize;
 	const void *data;
 
-	// transfer the framebuffer
-	// if (XCapture::nvIFR.nvIFROGLTransferFramebufferToHwEnc(
-	// 		m_hTransferObject, NULL, 0, GL_FRONT_LEFT, GL_NONE) != NV_IFROGL_SUCCESS)
-	if (XCapture::nvIFR.nvIFROGLTransferFramebufferToHwEnc(
-			m_hTransferObject, NULL, drawFBO, GL_BACK_LEFT, GL_NONE) != NV_IFROGL_SUCCESS)
+	if (XCapture::nvIFR.nvIFROGLTransferFramebufferToHwEnc(m_hTransferObject,
+		NULL, drawFBO, GL_BACK_LEFT, GL_NONE) != NV_IFROGL_SUCCESS)
 	{
 		throw_nvifr_error("Failed to transfer data from the framebuffer.");
 	}
 
-	fprintf(stderr, "Now lock transfer object.\n");
-
 	// lock the transferred data
 	if (XCapture::nvIFR.nvIFROGLLockTransferData(m_hTransferObject, &dataSize,
-												 &data) != NV_IFROGL_SUCCESS)
+		&data) != NV_IFROGL_SUCCESS)
 	{
 		THROW("Failed to lock the transferred data.");
 	}
@@ -116,10 +213,9 @@ void captureHwEnc()
 	{
 		THROW("Failed to release the transferred data.");
 	}
-	fprintf(stderr, "Done with captureHwEnc\n");
 }
 
-void captureSys()
+void GPUEncTrans::captureSys()
 {
 	GLint drawFBO;
 	glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &drawFBO);
@@ -127,29 +223,22 @@ void captureSys()
 	GLenum readBuffer;
 	glGetIntegerv(GL_READ_BUFFER, (GLint *)&readBuffer);
 
-	fprintf(stderr, "fbo is %d, readBuffer is %d\n", drawFBO, readBuffer);
-
 	uintptr_t dataSize;
 	const void *data;
 
-	// transfer the framebuffer
-	// if (XCapture::nvIFR.nvIFROGLTransferFramebufferToSys(
-	// 		m_hSysTransferObject, 0, GL_FRONT_LEFT, NV_IFROGL_TRANSFER_FRAMEBUFFER_FLAG_NONE, 0, 0, 0, 0) != NV_IFROGL_SUCCESS)
-	if (XCapture::nvIFR.nvIFROGLTransferFramebufferToSys(
-			m_hSysTransferObject, drawFBO, GL_BACK_LEFT, NV_IFROGL_TRANSFER_FRAMEBUFFER_FLAG_NONE, 0, 0, 0, 0) != NV_IFROGL_SUCCESS)
+	if (XCapture::nvIFR.nvIFROGLTransferFramebufferToSys(m_hSysTransferObject,
+		drawFBO, GL_BACK_LEFT, NV_IFROGL_TRANSFER_FRAMEBUFFER_FLAG_NONE, 0, 0, 0,
+		0) != NV_IFROGL_SUCCESS)
 	{
 		throw_nvifr_error("Failed to transfer data from the framebuffer.");
 	}
 
-	fprintf(stderr, "Now lock transfer object.\n");
-
 	// lock the transferred data
 	if (XCapture::nvIFR.nvIFROGLLockTransferData(m_hSysTransferObject, &dataSize,
-												 &data) != NV_IFROGL_SUCCESS)
+		&data) != NV_IFROGL_SUCCESS)
 	{
 		THROW("Failed to lock the transferred data.");
 	}
-	fprintf(stderr, "Done locking transfer object.\n");
 
 	// release the data buffer
 	if (XCapture::nvIFR.nvIFROGLReleaseTransferData(m_hSysTransferObject) !=
@@ -159,133 +248,96 @@ void captureSys()
 	}
 }
 
-// This just wraps the VGLTrans class in order to demonstrate how to build a
-// custom transport plugin for VGL and also to serve as a sanity check for the
-// plugin API
 extern "C"
 {
 
-	void *RRTransInit(Display *dpy, Window win_, FakerConfig *fconfig_)
+	void *RRTransInit(Display *dpy, Window win, FakerConfig *fconfig)
 	{
-		fprintf(stderr, "Hit RRTransInit!\n");
-
-		// fprintf(stderr, "Call CreateEncSession\n");
-
-		// setupNVIFRHwEnc();
-
-		memset(&rr_frame, 0, sizeof(RRFrame));
-		memset(&config, 0, sizeof(config));
-
-		return (void *)thing;
-	}
-
-	int RRTransConnect(void *handle, char *receiverName, int port)
-	{
-		fprintf(stderr, "RRTransConnect!\n");
-
+		void *retval = NULL;
 		try
 		{
-			XCapture::nvIFR.initialize();
-			if (XCapture::nvIFR.nvIFROGLCreateSession(&m_hSession, NULL) !=
-				NV_IFROGL_SUCCESS)
-			{
-				THROW("Failed to create a NvIFROGL session.");
-			}
-
-			m_fpOut = fopen("cool.h264", "wb");
-			if (m_fpOut == NULL)
-			{
-				THROW("Failed to create output file.");
-			}
-
-			// setupNVIFRSYS();
-		}
-		catch(Error &e)
-		{
-			err = e;  return -1;
-		}
-
-		fprintf(stderr, "Done RRTransConnect!\n");
-		// Return 0 for success
-		return 0;
-	}
-
-	RRFrame *RRTransGetFrame(void *handle, int width, int height, int format,
-							 int stereo)
-	{
-		get_frame_count += 1;
-		fprintf(stderr, "RRTransGetFrame! w: %d, h: %d, format: %d, stereo: %d. Call count: %d\n", width, height, format, stereo, get_frame_count);
-		if (!rr_frame.bits) {
-			rr_frame.w = width;
-			rr_frame.h = height;
-			rr_frame.pitch = width * rrtrans_ps[format];
-			rr_frame.bits = new unsigned char[height * rr_frame.pitch];
-			rr_frame.rbits = nullptr;
-			rr_frame.format = format;
-		}
-		try
-		{
-			setupNVIFRHwEnc(width, height);
+			retval = (void *)(new GPUEncTrans(dpy, win, fconfig));
 		}
 		catch(Error &e)
 		{
 			err = e;  return NULL;
 		}
+		return retval;
+	}
 
+	int RRTransConnect(void *handle, char *receiverName, int port)
+	{
+		// Return 0 for success
+		return 0;
+	}
 
-		return &rr_frame;
+	RRFrame *RRTransGetFrame(void *handle, int width, int height, int format,
+		int stereo)
+	{
+		GPUEncTrans *trans = (GPUEncTrans *)handle;
+		if(!trans) THROW("Invalid handle");
+		try
+		{
+			// trans->setupNVIFRSYS();
+			trans->setupNVIFRHwEnc(width, height);
+		}
+		catch(Error &e)
+		{
+			err = e;  return NULL;
+		}
+		return trans->getFrame(width, height, format);
 	}
 
 	int RRTransReady(void *handle)
 	{
-		fprintf(stderr, "RRTransReady!\n");
 		// Return 1 for "ready"
 		return 1;
 	}
 
 	int RRTransSynchronize(void *handle)
 	{
-		fprintf(stderr, "RRTransSynchronize!\n");
 		// Return 0 for success
 		return 0;
 	}
 
 	int RRTransSendFrame(void *handle, RRFrame *frame, int sync)
 	{
-		fprintf(stderr, "hmm RRTransSendFrame!\n");
-
-		// A session is required. The session is associated with the current OpenGL
-		// context.
-		// fprintf(stderr, "Start sleep\n");
-		// sleep(10);
-		// fprintf(stderr, "End sleep\n");
-		// captureSys();
 		try
 		{
-			captureHwEnc();
+			GPUEncTrans *trans = (GPUEncTrans *)handle;
+			if(!trans) THROW("Invalid handle");
+			// A session is required. The session is associated with the current
+			// OpenGL context.
+			// sleep(10);
+			// trans->captureSys();
+			trans->captureHwEnc();
 		}
 		catch(Error &e)
 		{
 			err = e;  return -1;
 		}
-
-		fprintf(stderr, "RRTransSendFrame Done!\n");
-
 		// Return 0 for success
 		return 0;
 	}
 
 	int RRTransDestroy(void *handle)
 	{
-		fprintf(stderr, "RRTransDestroy!\n");
-		delete [] rr_frame.bits;  rr_frame.bits = nullptr;
+		try
+		{
+			GPUEncTrans *trans = (GPUEncTrans *)handle;
+			if(!trans) THROW("Invalid handle");
+			delete trans;
+		}
+		catch(Error &e)
+		{
+			err = e;  return -1;
+		}
 		// Return 0 for success
 		return 0;
 	}
 
 	const char *RRTransGetError(void)
 	{
-		fprintf(stderr, "RRTransGetError!\n");
 		snprintf(errStr, MAXSTR + 14, "Error in %s -- %s",
 			err.getMethod(), err.getMessage());
 		return errStr;
